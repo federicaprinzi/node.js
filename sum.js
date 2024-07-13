a87de818225d3b58f03650be179bd6831ec210ec
_edera_ <fedecprinzi@gmail.com>
2024-07-13 17:08:08 +0200

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,7 @@
+//Exercise 1: Create a sum script
+//Create a script that outputs the total of 5 + 6 when you run it with Node.js.
+
+const a = 5;
+const b = 6;
+const sum = a + b;
+console.log(`La somma di ${a} e ${b} equivale a ${sum}`);
